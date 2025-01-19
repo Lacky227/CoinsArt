a3dd23ea70311b5a1644bedc135d3743a34749be
Lacky229 <tishko.artyr@gmail.com>
2025-01-19 14:15:04 +0200

    Connect database and add dependencies

---
+++ src/main/java/com/veedev/api/coinsart/CoinsartApplication.java
@@ -0,0 +1,13 @@
+package com.veedev.api.coinsart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinsartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinsartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veedev/api/coinsart/CoinsartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veedev.api.coinsart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinsartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
